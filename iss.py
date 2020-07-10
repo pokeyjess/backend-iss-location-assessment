285a9cc2e2578a8a26dc27c71a9e86ac38dc8c7c
Jessica Benson <pokeyjess72@gmail.com>
2020-07-10 13:40:29 -0400

    Put code into functions

--- iss.py
+++ iss.py
@@ -8,20 +8,34 @@ import urllib.request
 import time
 
 # ASTRONAUTS
-response = urllib.request.urlopen("http://api.open-notify.org/astros.json")
-result = json.loads(response.read())
-print("Total astronauts in space right now: " + str(result["number"]))
-astronauts = result["people"]
-for astronaut in astronauts:
-    print("Name: " + astronaut["name"] +
-          "\n \t Spacecraft: " + str(astronaut["craft"]))
+
+
+def get_astronauts():
+    response = urllib.request.urlopen("http://api.open-notify.org/astros.json")
+    result = json.loads(response.read())
+    print("Total astronauts in space right now: " + str(result["number"]))
+    astronauts = result["people"]
+    for astronaut in astronauts:
+        print("Name: " + astronaut["name"] +
+              "\n \t Spacecraft: " + str(astronaut["craft"]))
+
+
+get_astronauts()
+
 # LOCATION
-response = urllib.request.urlopen("http://api.open-notify.org/iss-now.json")
-result = json.loads(response.read())
-location = result["iss_position"]
-latitude = location["latitude"]
-longitude = location["longitude"]
-print("Latitude: " + str(latitude) + "\nLongitude: " + str(longitude))
+
+
+def get_location():
+    response = urllib.request.urlopen(
+        "http://api.open-notify.org/iss-now.json")
+    result = json.loads(response.read())
+    location = result["iss_position"]
+    latitude = location["latitude"]
+    longitude = location["longitude"]
+    print("Latitude: " + str(latitude) + "\nLongitude: " + str(longitude))
+
+
+get_location()
 
 
 def main():
