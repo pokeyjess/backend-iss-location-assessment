d01fa74c858bc2b88f523edf10d6042223825a5a
Jessica Benson <pokeyjess72@gmail.com>
2020-07-10 13:13:53 -0400

    Adds ISS location

--- iss.py
+++ iss.py
@@ -7,15 +7,21 @@ import turtle
 import urllib.request
 import time
 
-
-api = "http://api.open-notify.org/astros.json"
-response = urllib.request.urlopen(api)
+# ASTRONAUTS
+response = urllib.request.urlopen("http://api.open-notify.org/astros.json")
 result = json.loads(response.read())
 print("Total astronauts in space right now: " + str(result["number"]))
 astronauts = result["people"]
 for astronaut in astronauts:
     print("Name: " + astronaut["name"] +
           "\n \t Spacecraft: " + str(astronaut["craft"]))
+# LOCATION
+response = urllib.request.urlopen("http://api.open-notify.org/iss-now.json")
+result = json.loads(response.read())
+location = result["iss_position"]
+latitude = location["latitude"]
+longitude = location["longitude"]
+print("Latitude: " + str(latitude) + "\nLongitude: " + str(longitude))
 
 
 def main():
