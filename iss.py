82618481b487e162fe338dc5063061aac057c7ed
Jessica Benson <pokeyjess72@gmail.com>
2020-07-12 14:18:24 -0400

    Refactors to add location to map

--- iss.py
+++ iss.py
@@ -6,14 +6,14 @@ import json
 import turtle
 import requests
 import time
-import argparse
+#import argparse
 
-# ASTRONAUTS
+astronaut_response = requests.get("http://api.open-notify.org/astros.json")
+location_response = requests.get("http://api.open-notify.org/iss-now.json")
 
 
 def get_astronauts():
-    response = requests.get("http://api.open-notify.org/astros.json")
-    result = response.json()
+    result = astronaut_response.json()
     number = result["number"]
     print("Total astronauts in space right now: " + str(number))
     astronauts = result["people"]
@@ -24,28 +24,44 @@ def get_astronauts():
 
 get_astronauts()
 
-# LOCATION
+
+def get_longitude():
+    result = location_response.json()
+    location = result["iss_position"]
+    longitude = location["longitude"]
+    return int(float(longitude))
 
 
-def get_location():
-    response = requests.get("http://api.open-notify.org/iss-now.json")
-    result = response.json()
+def get_latitude():
+    result = location_response.json()
     location = result["iss_position"]
     latitude = location["latitude"]
-    longitude = location["longitude"]
-    print("Latitude: " + str(latitude) + "\nLongitude: " + str(longitude))
+    return int(float(latitude))
 
 
-get_location()
+print(f"Longitude: " + str(get_longitude()) +
+      "\nLatitude: " + str(get_latitude()))
 
 
-map = turtle.Screen()
-map.setup(800, 400)  # width, height
-map.setworldcoordinates(-180, -90, 180, 90)
-map.bgpic("map.gif")
+def create_map():
+    map = turtle.Screen()
+    map.setup(800, 400)  # width, height
+    map.setworldcoordinates(-180, -90, 180, 90)
+    map.bgpic("map.gif")
+    map.register_shape("rocket.gif")
+
+
+create_map()
+
+iss = turtle.Turtle()
+iss.shape("rocket.gif")
+iss.setheading(90)
+iss.penup()
+iss.goto(get_longitude(), get_latitude())
 
 
 turtle.done()  # last turtle command to keep graphic open
+
 
 # part D
 # how to pass proper parameters for Indy into api through requests package
